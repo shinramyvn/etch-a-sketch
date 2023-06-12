aceb82ce8aebba5c7c660a20763bf2c2e2104440
Vincent Nguyen <vincentmknguyen@gmail.com>
2023-06-12 12:18:23 -0700

    Added change grid size and RBG drawing There are now buttons to change how many cells are inside of your grid. You can now also change drawing color from black to rainbow rgb value each pass through.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let GRID_SIZE = 24;
+let DRAW_COLOR = 'black';
+
 const container = document.querySelector('#container');
 
 function makeGrid(rows, cols) {
@@ -10,12 +13,65 @@ function makeGrid(rows, cols) {
         }
     }
 }
-makeGrid(50,50);
+makeGrid(GRID_SIZE,GRID_SIZE);
 
 const cells = document.querySelectorAll('.cell');
 
 cells.forEach((cell) => {
     cell.addEventListener('mouseover', () => {
-        cell.style.backgroundColor = "black";
+        if(DRAW_COLOR === 'black') {
+            DRAW_COLOR = 'black';
+        } else {
+            DRAW_COLOR = rainbow();
+        }
+        cell.style.backgroundColor = DRAW_COLOR;
     });
 })
+
+const resize = document.querySelectorAll('.resize-container button');
+
+resize.forEach((button) => {
+    button.addEventListener('click', () => {
+        GRID_SIZE = button.id;
+        resetGrid()
+    });
+})
+
+
+function rainbow() {
+    return 'rgb('+randomNumber(255)+','+randomNumber(255)+','+randomNumber(255)+')';
+}
+
+
+function randomNumber(max) {
+    return Math.floor(Math.random()*max+1);
+}
+const changeColor = document.querySelectorAll('.color-changer button');
+
+changeColor.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (button.id === 'black') {
+            DRAW_COLOR = 'black';
+        } else if (button.id === 'rgb') {
+            DRAW_COLOR = 'rbg';
+        }
+    });
+})
+
+
+function resetGrid() {
+    container.innerHTML = "";
+    makeGrid(GRID_SIZE,GRID_SIZE);
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            if(DRAW_COLOR === 'black') {
+                DRAW_COLOR = 'black';
+            } else {
+                DRAW_COLOR = rainbow();
+            }
+            cell.style.backgroundColor = DRAW_COLOR;
+        });
+    })
+}
